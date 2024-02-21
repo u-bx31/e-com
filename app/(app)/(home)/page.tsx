2903fb6e7871f2b88be74ea4a0afa065011e567d
LabchariOussama <oussama.bx31@gmail.com>
2024-02-21 14:39:34 +0100

    wip

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/shared/Navbar";
 import HeroSection from "./_components/HeroSection";
 import BestSellingSection from "./_components/BestSellingSection";
 import NewProductsSection from "./_components/NewProductsSection";
@@ -9,7 +8,7 @@ import ServicesSection from "./_components/ServicesSection";
 
 export default function Home() {
 	return (
-		<div className="md:container px-2 flex flex-col gap-9 md:gap-14 mb-40">
+		<div className="md:container px-2 flex flex-col gap-9 md:gap-14 mb-14">
 			<HeroSection />
 			<BestSellingSection />
 			<NewProductsSection />
---
+++ app/(app)/categories/page.tsx
@@ -0,0 +1,9 @@
+const CategoriesPage = () => {
+	return (
+		<div className="md:container px-2 flex flex-col gap-9 md:gap-14 mb-14">
+			<h1></h1>
+		</div>
+	);
+};
+
+export default CategoriesPage;
